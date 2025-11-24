5f3f6545e147d97e27abfa3951d5249be654945a
goNiki <gogolevnikita123@gmail.com>
2025-11-24 09:04:34 +0300

    refactor: Добавленна колонка  team_id в таблицу pull_requests

--- migrations/20251123014505_create_table_pull_requests.sql
+++ migrations/20251123014505_create_table_pull_requests.sql
@@ -5,6 +5,7 @@ CREATE TABLE pull_requests (
     pr_id VARCHAR(50) PRIMARY KEY,
     pr_name VARCHAR(255) NOT NULL,
     author_id VARCHAR(50) NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    team_id INTEGER NOT NULL REFERENCES teams(id) ON DELETE CASCADE,
     status pr_status NOT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     merged_at TIMESTAMP
